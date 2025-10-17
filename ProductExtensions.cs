f8ab49b3a0f7f7fd384d662c16688452f15ddc94
DivanFerrari <104350966+DivanFerrari@users.noreply.github.com>
2025-10-17 10:39:19 +0200

    Add project files.

---
+++ ProductExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjInventoryManagement
+{
+    public static class ProductExtensions
+    {
+        public static double CalculateTotalValue(this List<Product> products)
+        {
+            return products.Sum(p => p.Quantity * p.Price);
+        }
+
+        public static List<Product> LowStock(this List<Product> products, int minQuantity)
+        {
+            return products.Where(p => p.Quantity >= minQuantity).ToList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace prjInventoryManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>  
+            {
+                new Product { ID = 1, ProductName = "Laptop", Quantity = 10, Price = 1000 },
+                new Product { ID = 2, ProductName = "Smartphone", Quantity = 25, Price = 600.00 },
+                new Product { ID = 3, ProductName = "Tablet", Quantity = 5, Price = 250.00 },
+                new Product { ID = 4, ProductName = "Headphones", Quantity = 50, Price = 100.00 },
+                new Product { ID = 5, ProductName = "Gaming Console", Quantity = 2, Price = 12000.00 }
+            };
+
+            double totalValue = products.CalculateTotalValue();
+            Console.WriteLine($"-----Total Inventory Value: R{totalValue}-----");
+
+            Console.WriteLine();
+
+            var lowStockProducts = products.LowStock(5);
+            Console.WriteLine("-----Products with Low Stock (less or equal to 5)-----");
+
+            foreach (var product in lowStockProducts)
+            {
+                Console.WriteLine($"- {product.ProductName} (Quantity: {product.Quantity})");
+            }
+
+            var selection = from p in products
+                            where p.Price > 300
+                            select p;
+
+            Console.WriteLine();
+
+            Console.WriteLine("-----Products with Price greater than R300-----");
+            foreach (var product in selection)
+            {
+                Console.WriteLine($"{product.ProductName} with a Price of R{product.Price})");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
